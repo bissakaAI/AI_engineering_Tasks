3235e9cd99eb0b3f66601f5bab4316d09ce1acf9
Hamid <bissakabissakadop31@gmail.com>
2025-08-12 15:07:33 +0100

    update on task 2

--- week_2/DataStructure/Hamid_Jimoh_task4.py
+++ week_2/DataStructure/Hamid_Jimoh_task4.py
@@ -1,14 +1,24 @@
-#TASK1
-#YOUR FAVOURITE LIFE QUOTE
-user_input = input ("hi user kindly input your favourite quote: ")
-print(f"\"{user_input.title()}\"")
+# #TASK1
+# #YOUR FAVOURITE LIFE QUOTE
+# user_input = input ("hi user kindly input your favourite quote: ")
+# print(f"\"{user_input.title()}\"")
 
-#TASK2
-#shopping list manager 
-Empty_list =[]
-shoping_items1 =input("please input what you are shopping: ")
-shoping_items2 =input("please input what you are shopping next: ")
-shoping_items3 =input("please input what you are shopping next: ")
+# #TASK2
+# #shopping list manager 
+# Empty_list =[]
+# shoping_items1 =input("please input what you are shopping: ")
+# shoping_items2 =input("please input what you are shopping next: ")
+# shoping_items3 =input("please input what you are shopping next: ")
+# shopping_list=[shoping_items1,shoping_items2,shoping_items3]
+# Empty_list=shopping_list.copy()
+# print(Empty_list)
 
-Empty_list.append(f"{shoping_items1},{shoping_items2},{shoping_items3}")
-print(Empty_list)
+# #TASK3
+# #WORD COUNTER
+# user_sentense = input ("Hey user kindly input your sentense ")
+# user_sentensesplited = user_sentense.split()
+# word_count = user_sentensesplited.index(user_sentensesplited[-1]) +1 
+# print (word_count)
+
+#task4
+#name organizer 
