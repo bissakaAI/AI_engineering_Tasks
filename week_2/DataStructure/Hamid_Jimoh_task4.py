0f9238c82eef8e1a0acf83a4e5e52ed27a050d34
Hamid <bissakabissakadop31@gmail.com>
2025-08-12 15:24:16 +0100

    new addition task4

--- week_2/DataStructure/Hamid_Jimoh_task4.py
+++ week_2/DataStructure/Hamid_Jimoh_task4.py
@@ -20,5 +20,15 @@
 # word_count = user_sentensesplited.index(user_sentensesplited[-1]) +1 
 # print (word_count)
 
-#task4
-#name organizer 
+# #task4
+# #name organizer 
+# user_input = input("please input the name of students and  seperate with spaces not commas: ").lower()
+
+#TASK5
+#student score tracker
+user_input = input("please input the name of students: ")
+user_input_list = user_input.split()
+for x in user_input_list:
+   scores= list(input(f"what is{x} score: "))
+print(f"{user_input_list}\n{scores}")
+
