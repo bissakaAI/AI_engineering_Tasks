05f07f0e3e04381af570cdcca2f107a24eebde0c
Hamid <bissakabissakadop31@gmail.com>
2025-08-12 14:39:31 +0100

    update with assignment

---
+++ week_2/DataStructure/Hamid_Jimoh_task4.py
@@ -0,0 +1,14 @@
+#TASK1
+#YOUR FAVOURITE LIFE QUOTE
+user_input = input ("hi user kindly input your favourite quote: ")
+print(f"\"{user_input.title()}\"")
+
+#TASK2
+#shopping list manager 
+Empty_list =[]
+shoping_items1 =input("please input what you are shopping: ")
+shoping_items2 =input("please input what you are shopping next: ")
+shoping_items3 =input("please input what you are shopping next: ")
+
+Empty_list.append(f"{shoping_items1},{shoping_items2},{shoping_items3}")
+print(Empty_list)
