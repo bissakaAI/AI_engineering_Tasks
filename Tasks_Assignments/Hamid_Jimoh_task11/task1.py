6a3841d6de4a08039137f6a369164ec9b4d7168c
bissakaAI <bissakabissakadop31.com>
2025-08-28 23:43:03 +0100

    task11done

--- Tasks_Assignments/Hamid_Jimoh_task11/task1.py
+++ Tasks_Assignments/Hamid_Jimoh_task11/task1.py
@@ -1,5 +1,71 @@
+from pathlib import Path
+import csv
 #Take the name  and phone number and track and age 
+#INPUT
+dictionary={}
+def participant_details():
+    Participant_name = input("Good day Participant.What is your name")
+    if Participant_name == "":
+        print("You have to input your name")
+    else:
+        dictionary["Name"]= Participant_name
+    try:
+        Age = int(input("Input your Age in years"))
+        if Age == "":
+            print("You have to input your Age")
+        else:
+            dictionary["Age"]=Age
+    except  ValueError:
+        print("Error: You have to input a Number")
+    except Exception as e:
+        print (e)
 
-Participant_name = input("Good day Participant.What is your name")
-Age = int(input("Input your Age in years"))
-Track = input("Which Track are you enrolled in")
+    Track = input("Which Track are you enrolled in")
+    dictionary["Track "]=Track
+
+    phone_number = int(input("Please input your phone number "))
+
+    if (len(str(phone_number))+1) <11:
+        print("Invalid input!! Phone number needs to be 11 digits")
+    elif (len(str(phone_number))+1) >11:
+        print("Invalid input!! Phone number needs to be 11 digits\n You have inputed more than 11 digits")
+    else:
+        dictionary["Phone Number"]=phone_number
+    print("Thank you for Inputing your details. Your data has been saved ")
+    print(dictionary)
+participant_details()
+curren_directory1 =Path.cwd()
+curren_directory = curren_directory1/"Tasks_Assignments/Hamid_Jimoh_task11"
+file_name = curren_directory/Path("csvfolder")
+file_name.mkdir(exist_ok=True)
+csv_file = file_name/"task1.csv"
+
+#to append the details of input to contack_details but as a dictionary
+# dictionary = {"Name: ":name, "Age": Age, "Phone Number": phone_number, "Track ":Track}
+
+contact_detail = [    ]
+contact_details = [  ['Name','Age','Track' ,'Phone Number'],  ]
+
+for key,value in dictionary.items():
+    contact_detail.append(value)
+
+contact_details.append(contact_detail)
+    
+
+
+# #CREATING FILE
+# print(contact_details)
+# with open(csv_file,"w",newline="", encoding="utf-8") as f:
+#      writer = csv.writer(f)
+#      writer.writerows(contact_details)
+
+with open(csv_file, "r", newline="" ,encoding="utf-8") as f:
+    writer = csv.reader(f)
+    for row_num, row in enumerate(writer):
+        if row_num== 0:
+            print(f"Headers: {'|'.join(row)}")
+            print("-"*40)
+        else: #Data rows
+            name,age,Track,Phone_number =row
+            print(f"{name} ({age} years) - {Track} --- {Phone_number}")
+
--- week_4/working _with_files/working_with_files2.py
+++ week_4/working _with_files/working_with_files2.py
@@ -6,205 +6,205 @@ workspace.mkdir(exist_ok =True)
 file_path = workspace/"notes.txt"
 
 
-# f = open(file_path,"w",encoding="utf-8")
-# f.write("This is the journey of my AI career")
-# f.close
+f = open(file_path,"w",encoding="utf-8")
+f.write("This is the journey of my AI career")
+f.close
 
-# # #(B) Safe-create using 'x' (uncomment to try once)
-# # f = open(workspace / "created_once.txt", "x", encoding="utf-8")
-# # f.write("This file will only be created if it doesn't exist.\n")
-# # f.close()
-
-# f = open(file_path,"w",encoding="utf-8")
-# f.write("check items below \n")
-# f.write("Apple \n")
-# f.write("clocking system \n")
-# f.write("banana \n")
+# #(B) Safe-create using 'x' (uncomment to try once)
+# f = open(workspace / "created_once.txt", "x", encoding="utf-8")
+# f.write("This file will only be created if it doesn't exist.\n")
 # f.close()
 
-
-# f=open(file_path,"a", encoding="utf-8")
-# f.write("This is the next line in this file\n")
-# f.write("Maggi \n")
-# f.close()
-
-# f = open(file_path,"r",encoding="utf-8")
-# content =f.read()
-# print(content)
-# f.close
+f = open(file_path,"w",encoding="utf-8")
+f.write("check items below \n")
+f.write("Apple \n")
+f.write("clocking system \n")
+f.write("banana \n")
+f.close()
 
 
-# #Read line-by-line
-# f = open(file_path,"r",encoding="utf-8")
-# print("firstline:", f.readline().rstrip())
-# print("second line:",f.readline().rstrip())
-# f.close()
+f=open(file_path,"a", encoding="utf-8")
+f.write("This is the next line in this file\n")
+f.write("Maggi \n")
+f.close()
 
-# f= open(file_path,"r",encoding="utf-8")
-# list = f.readlines()
-# f.close()
-# print("lines list:",[line.rstrip() for line in list])
-
-
-# f = open(file_path, "r", encoding="utf-8")
-# for line in f:
-#     print("-->", line.rstrip())
-# f.close()
-
-# #using the open() as f :best practice 
-# #this automatically closes the file even if an error happens .
-# #write safely
-# with open(file_path,"w", encoding= "utf-8") as f:
-#     f.write("My to-do list\n")
-#     f.write("-Revise python file handling\n")
-#     f.write("-Practice error handling within a function\n")
-#     f.write("-Practice JSON & CSV\n")
+f = open(file_path,"r",encoding="utf-8")
+content =f.read()
+print(content)
+f.close
 
 
-# with open(file_path,"a", encoding= "utf-8") as f:
-#     f.write("--> Build a small project\n")
+#Read line-by-line
+f = open(file_path,"r",encoding="utf-8")
+print("firstline:", f.readline().rstrip())
+print("second line:",f.readline().rstrip())
+f.close()
+
+f= open(file_path,"r",encoding="utf-8")
+list = f.readlines()
+f.close()
+print("lines list:",[line.rstrip() for line in list])
 
 
-# try:
-#     with open(workspace /"missing_file.txt", "r") as f:
-#         content = f.read()
-#         print("file content: ", content)
-# except FileNotFoundError:
-#     print("oops! That file doesnt exist yet")
-#     print("lets create it first ")
+f = open(file_path, "r", encoding="utf-8")
+for line in f:
+    print("-->", line.rstrip())
+f.close()
 
-#     #now create the file
-#     with open(workspace/"missing_file.txt","w",)as f:
-#         f.write("Now i exist!")
-#     print("File created successfully")
+#using the open() as f :best practice 
+#this automatically closes the file even if an error happens .
+#write safely
+with open(file_path,"w", encoding= "utf-8") as f:
+    f.write("My to-do list\n")
+    f.write("-Revise python file handling\n")
+    f.write("-Practice error handling within a function\n")
+    f.write("-Practice JSON & CSV\n")
 
 
+with open(file_path,"a", encoding= "utf-8") as f:
+    f.write("--> Build a small project\n")
 
 
-# #check if our file exists 
-# if file_path.exists():
-#     print(f"Found the file: {file_path.name}")
+try:
+    with open(workspace /"missing_file.txt", "r") as f:
+        content = f.read()
+        print("file content: ", content)
+except FileNotFoundError:
+    print("oops! That file doesnt exist yet")
+    print("lets create it first ")
 
-#     #Get some information ABOUT THE FILE 
-#     file_size = file_path.stat().st_size
-#     print(f"File size: {file_size} bytes")
-
-#     #Read and show the content 
-#     with open(file_path, "r", encoding ="utf-8") as f:
-#         content = f.read()
-#         print(f"content preview: {content[:50]}...")
-# else: 
-#     print(f"file {file_path.name} doesn't exist")
-#     print("you might want to create it first!")
-
-# #if notes.txt exists 
+    #now create the file
+    with open(workspace/"missing_file.txt","w",)as f:
+        f.write("Now i exist!")
+    print("File created successfully")
 
 
 
 
+#check if our file exists 
+if file_path.exists():
+    print(f"Found the file: {file_path.name}")
 
-# #create some student data (like a mini database )
-# student_data = {
-#     "name": "Oyindamola",
-#      "age": 16,
-#      "courses": ["Python","Data Science","Web Development  "],
-#      "grades": {"Python":"A","Data Science": "B+", "Web Development": "A-"},
-#      "is_graduated": False
-# }
+    #Get some information ABOUT THE FILE 
+    file_size = file_path.stat().st_size
+    print(f"File size: {file_size} bytes")
 
+    #Read and show the content 
+    with open(file_path, "r", encoding ="utf-8") as f:
+        content = f.read()
+        print(f"content preview: {content[:50]}...")
+else: 
+    print(f"file {file_path.name} doesn't exist")
+    print("you might want to create it first!")
 
-# #save the data to a json file 
-# json_file = workspace / "student_data.json"
-# with open(json_file, "w", encoding= "utf-8") as f:
-#     json.dump(student_data, f, indent= 2)
-# print("Student data saved to JSON file!")
-
-# #Now read it back 
-# with open(json_file, "r", encoding= "utf-8") as f :
-#     loaded_data = json.load(f)
-
-# print("\n Data read from JSON file ")
-# print(f"Student name: {loaded_data["name"]}")
-# print(f"Age: {loaded_data["age"]}")
-# print(f"Courses: {",".join(loaded_data["courses"])}")
-# print(f"Python grade: {loaded_data['grades']['Python']}")
+#if notes.txt exists 
 
 
 
 
-# workjspace = Path("workspace_files")
-# csv_file = workspace/"students.csv"
 
-# #create sample student data 
-# students = [
-#     ["Name", "Age", "Course", "Grade"],  # Header row
-#     ["Precious", 20, "Python", "A"],
-#     ["Favour", 22, "JavaScript", "B+"],
-#     ["Ore", 21, "Python", "A-"],
-#     ["Susan", 23, "Data Science", "A"]
-# ]
+#create some student data (like a mini database )
+student_data = {
+    "name": "Oyindamola",
+     "age": 16,
+     "courses": ["Python","Data Science","Web Development  "],
+     "grades": {"Python":"A","Data Science": "B+", "Web Development": "A-"},
+     "is_graduated": False
+}
 
-# #wrrite data to a csv file
-# with open(csv_file,"w",newline="", encoding= "utf-8") as f :
-#     writer = csv.writer(f)
-#     writer.writerows(students) #write all rows at once m
 
-# print("Student data written to csv file")
+#save the data to a json file 
+json_file = workspace / "student_data.json"
+with open(json_file, "w", encoding= "utf-8") as f:
+    json.dump(student_data, f, indent= 2)
+print("Student data saved to JSON file!")
 
-# # Read the CSV file back
-# print("\nReading CSV file:")
-# with open(csv_file, "r", encoding="utf-8") as f:
-#     reader = csv.reader(f)
-#     for row_number,row in enumerate(reader):
-#         if row_number== 0:
-#             print(f"Headers: {'|'.join(row)}")
-#             print("-"*40)
-#         else: #Data rows
-#             name,age, course,grade =row
-#             print(f"{name} ({age} years) - {course}: {grade}")
+#Now read it back 
+with open(json_file, "r", encoding= "utf-8") as f :
+    loaded_data = json.load(f)
+
+print("\n Data read from JSON file ")
+print(f"Student name: {loaded_data["name"]}")
+print(f"Age: {loaded_data["age"]}")
+print(f"Courses: {",".join(loaded_data["courses"])}")
+print(f"Python grade: {loaded_data['grades']['Python']}")
 
 
 
-# #working with multiple files at once 
-# from pathlib import Path
-# workspace= Path("workspace_files")
-# input_file = workspace/"original.txt"
-# output_file = workspace/ "processed.txt"
 
-# #create an input file with some text
-# sample_text = """hello world
-# python programming
-# file handling
-# file handling tutorial
-# learning is fun"""
-# with open(input_file,"w", encoding="utf-8") as f:
-#     f.write(sample_text)
-# print("created input file")
+workspace = Path("workspace_files")
+csv_file = workspace/"students.csv"
 
-# # with open(output_file, "w", encoding="utf-8") as f:
-# #     print("output ok")
+#create sample student data 
+students = [
+    ["Name", "Age", "Course", "Grade"],  # Header row
+    ["Precious", 20, "Python", "A"],
+    ["Favour", 22, "JavaScript", "B+"],
+    ["Ore", 21, "Python", "A-"],
+    ["Susan", 23, "Data Science", "A"]
+]
 
-# #process the file: read from input, write processes version to output 
-# with open(input_file,"r",encoding="utf-8") as infile,\
-#      open(output_file,"w", encoding="utf-8") as outfile:
+#wrrite data to a csv file
+with open(csv_file,"w",newline="", encoding= "utf-8") as f :
+    writer = csv.writer(f)
+    writer.writerows(students) #write all rows at once m
+
+print("Student data written to csv file")
+
+# Read the CSV file back
+print("\nReading CSV file:")
+with open(csv_file, "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    for row_number,row in enumerate(reader):
+        if row_number== 0:
+            print(f"Headers: {'|'.join(row)}")
+            print("-"*40)
+        else: #Data rows
+            name,age, course,grade =row
+            print(f"{name} ({age} years) - {course}: {grade}")
+
+
+
+#working with multiple files at once 
+from pathlib import Path
+workspace= Path("workspace_files")
+input_file = workspace/"original.txt"
+output_file = workspace/ "processed.txt"
+
+#create an input file with some text
+sample_text = """hello world
+python programming
+file handling
+file handling tutorial
+learning is fun"""
+with open(input_file,"w", encoding="utf-8") as f:
+    f.write(sample_text)
+print("created input file")
+
+# with open(output_file, "w", encoding="utf-8") as f:
+#     print("output ok")
+
+#process the file: read from input, write processes version to output 
+with open(input_file,"r",encoding="utf-8") as infile,\
+     open(output_file,"w", encoding="utf-8") as outfile:
     
-#     line_number = 1 
-#     for line in infile:
-#         #Process eachline:make it uppercase and add line numbers 
-#         processed_line =f"line {line_number}: {line.strip().upper()}\n"
-#         outfile.write(processed_line)
-#         line_number +=1
+    line_number = 1 
+    for line in infile:
+        #Process eachline:make it uppercase and add line numbers 
+        processed_line =f"line {line_number}: {line.strip().upper()}\n"
+        outfile.write(processed_line)
+        line_number +=1
 
-# print("File processing complete!")
+print("File processing complete!")
 
-# # Show the results
-# print("\nOriginal file:")
-# with open(input_file, "r", encoding="utf-8") as f:
-#     print(f.read())
+# Show the results
+print("\nOriginal file:")
+with open(input_file, "r", encoding="utf-8") as f:
+    print(f.read())
 
-# print("\nProcessed file:")
-# with open(output_file, "r", encoding="utf-8") as f:
-#     print(f.read())
+print("\nProcessed file:")
+with open(output_file, "r", encoding="utf-8") as f:
+    print(f.read())
 
 
 
