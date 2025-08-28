1a72a2ff9786bab0cabbcde07ec74ba82fc9832c
Hamid <bissakabissakadop31@gmail.com>
2025-08-28 15:57:12 +0100

    start task 11

---
+++ Tasks_Assignments/Hamid_Jimoh_task11/task1.py
@@ -0,0 +1,5 @@
+#Take the name  and phone number and track and age 
+
+Participant_name = input("Good day Participant.What is your name")
+Age = int(input("Input your Age in years"))
+Track = input("Which Track are you enrolled in")
