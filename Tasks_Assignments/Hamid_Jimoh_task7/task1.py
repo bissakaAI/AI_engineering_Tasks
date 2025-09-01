9810f285157d656871419445d8517b67678b8ae0
bissakaAI <bissakabissakadop31.com>
2025-09-01 19:03:21 +0100

    home

--- Tasks_Assignments/Hamid_Jimoh_task7/task1.py
+++ Tasks_Assignments/Hamid_Jimoh_task7/task1.py
@@ -1,10 +1,12 @@
 database ={
 
 }
-user_input={}
-database.update({user_input["Name"] : user_input})
-courses=[]
+
+
+
 for i in range (1,3):
+    user_input={}
+    courses=[]
     user_name= input(f"input name your name: ")
     user_age = int(input(f"input name your age: "))
     user_gender = input(f"input name your gender: ")
@@ -16,7 +18,11 @@ for i in range (1,3):
     user_input["Name"]=user_name
     user_input["Gender"]=user_gender
     user_input["age"]=user_age
-
+    print(user_input)
+    database_key =user_name
+    database_value= user_input
+    database[database_key] = database_value
+    print(database)
 
 for key,value in database.items():
     print(f"{key}:\t{value}\n")
