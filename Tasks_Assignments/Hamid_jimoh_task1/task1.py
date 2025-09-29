c340a77aeb78e725dbc2bbc1ec7d8de1db406046
bissakaAI <bissakabissakadop31.com>
2025-09-29 18:08:21 +0100

    linksubmit1

---
+++ Tasks_Assignments/Hamid_jimoh_task1/task1.py
@@ -0,0 +1,38 @@
+#Task 1
+#Write a python program that prints the following information on separate lines: Your full name, Your uni, Your local government are, and Your favourite Nigerian food
+print("Full name: Jimoh Hamid \nUni: Afe Babalola University \nLGA: Kobape\nFavourite Nigerina food: Amala")
+
+
+
+#Task 2
+#Write a Python program that stores your name and state of origin in variables, and print a message
+name = "Jimoh HAmid"
+StateOfOrigin = "Oyo State"
+print (f"Hello {name}, Good to know that you're from {StateOfOrigin}")
+
+
+
+
+#Task 3
+#Print a simple timetable for a day in a Nigerian secondary school using tab spacing and newlines
+print("\t\t\t JSS3 Timetable")
+print("Day\t\t Period 1\t\t Period 2\t\t Period 3 \nMonday\t\t Math\t\t\t English\t\t Geography \nTuesday\t\t Yoruba\t\t\t Basic S\t\t TD")
+
+
+
+
+# #Task 4
+# #Write a Python program that uses variables to store: Your name, Your class, Your best subject. Then use an f-string to format and print them in a sentence
+name = input("Enter your name:")
+Class = input("Enter your class: ")
+subject = input("Enter your best course: ")
+print(f"Hello {name} you are in class {Class} and your best subject is {subject}\n")
+
+
+
+
+
+
+#Task 5
+#Write a short 3-line poem about Nigerian and print it using triple quotes (""" """)
+print('\"""Nigeria is green Nigeria is white Nigeria is green and white\"""')
